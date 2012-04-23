1f6c17a88ecc23c4b9fe20ce7b8a5b904b1bd898
Sean O'Leary <soleary@solutionsforprogress.com>
2012-04-23 16:30:37 -0400

    Almost a solution to jokers wild

--- unit1/homework/jokers-wild.py
+++ unit1/homework/jokers-wild.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # CS 212, hw1-2: Jokers Wild
 #
 # -----------------
@@ -26,19 +27,32 @@
 
 import itertools
 
+ranks = list("2 3 4 5 6 7 8 9 T J Q K A".split())
+red_suits = [ 'H', 'D' ]
+black_suits = [ 'S', 'C' ]
+
+jokers = {}
+
+jokers['R'] = [ r + s for s in red_suits for r in ranks ]
+jokers['B'] = [ r + s for s in black_suits for r in ranks ]
+
 def best_wild_hand(hand):
     "Try all values for jokers in all 5-card selections."
-    
-    # Your code here
 
-def test_best_wild_hand():
-    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
-            == ['7C', '8C', '9C', 'JC', 'TC'])
-    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
-            == ['7C', 'TC', 'TD', 'TH', 'TS'])
-    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
-            == ['7C', '7D', '7H', '7S', 'JD'])
-    return 'test_best_wild_hand passes'
+    print hand
+    wildhand = itertools.new()
+    #c = [ jokers[ c[1] ] for c in hand if c.find('?') == 0 ]
+    for c in hand:
+        if c.find('?') == 0:
+            wildhand.chain(jokers[c[1]])
+        else:
+            wildhand.append(c)
+    
+    print wildhand
+    wildhand = itertools.chain(wildhand)
+    print wildhand
+
+    return max(itertools.combinations(hand, 5), key=hand_rank)
 
 # ------------------
 # Provided Functions
@@ -68,7 +82,7 @@ def hand_rank(hand):
         return (1, kind(2, ranks), ranks)
     else:
         return (0, ranks)
-    
+
 def card_ranks(hand):
     "Return a list of the ranks, sorted with higher first."
     ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
@@ -101,5 +115,16 @@ def two_pair(ranks):
     if pair and lowpair != pair:
         return (pair, lowpair)
     else:
-        return None 
+        return None
+
+def test_best_wild_hand():
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
+print test_best_wild_hand()
 
