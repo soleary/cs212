82ab47fe1b34e2c22f7bc8c8d5338ff0b606a830
Sean O'Leary <sean@soleary.com>
2012-05-14 00:50:40 -0400

    Initial states for Unit 5 exercises

---
+++ unit5/exercises/hold-at.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -----------------
+# User Instructions
+# 
+# In this problem, you will complete the code for the hold_at(x) 
+# function. This function returns a strategy function (note that 
+# hold_at is NOT the strategy function itself). The returned 
+# strategy should hold if and only if pending >= x or if the 
+# player has reached the goal.
+
+def hold_at(x):
+    """Return a strategy that holds if and only if 
+    pending >= x or player reaches goal."""
+    def strategy(state):
+        # your code here
+    strategy.__name__ = 'hold_at(%d)' % x
+    return strategy
+
+def test():
+    goal = 50
+    assert hold_at(30)((1, 29, 15, 20)) == 'roll'
+    assert hold_at(30)((1, 29, 15, 21)) == 'hold'
+    assert hold_at(15)((0, 2, 30, 10))  == 'roll'
+    assert hold_at(15)((0, 2, 30, 15))  == 'hold'
+    return 'tests pass'
+
+print test()
+
+
